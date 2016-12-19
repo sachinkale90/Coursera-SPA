ec780bea128b21a08012d4e4a8575a110f3ce67d
sachinkale <sunnysachin15@gmail.com>
2016-12-18 23:53:01 -0700

    modified to use controller as syntax

--- assignments/module2-solution/app.js
+++ assignments/module2-solution/app.js
@@ -3,40 +3,52 @@
   var app = angular.module('ShoppingApp',[]);
   app.controller('ParentShoppingController',ParentShoppingController);
   app.controller('ChildShoppingController',ChildShoppingController);
-var toBuyList = [
-  {name:"Milk", quantity :10},
-  {name:"Donuts", quantity :12},
-  {name:"Pepto Bismol", quantity :5},
-  {name:"Cookies", quantity :20},
-  {name:"Chocolate", quantity :10},
-];
-var boughtItemsList = [];
- ParentShoppingController.$inject = ['$scope'];
- function ParentShoppingController($scope){
-   $scope.toBuyItems = toBuyList;
-   $scope.boughtItemsList = boughtItemsList;
-   $scope.AlreadyBroughtMessage = 'Nothing bought yet';
-   $scope.ToBuyMessage = '';
-   $scope.AddToBroughtList = function(index){
-     var newItem = {
-           name: $scope.toBuyItems[index].name,
-           quantity: $scope.toBuyItems[index].quantity
-         };
-         $scope.boughtItemsList.push(newItem);
-         $scope.toBuyItems.splice(index,1);
-         if($scope.toBuyItems.length === 0){
-           $scope.ToBuyMessage = 'Everything is bought';
-         }
-         $scope.$broadcast('parent',$scope.boughtItemsList.length);
+  app.service('ShoppingListCheckOffService',ShoppingListCheckOffService);
+
+
+ ParentShoppingController.$inject = ['ShoppingListCheckOffService'];
+ function ParentShoppingController(ShoppingListCheckOffService){
+   var parent = this;
+   parent.message = '';
+   parent.items = ShoppingListCheckOffService.getItemsToBuy();
+   parent.AddToBroughtList = function (item){
+      ShoppingListCheckOffService.buyingItem(item);
+      if(parent.items.length === 0){
+      parent.message = 'Everything is bought';
+     }
    }
  };
- ChildShoppingController.$inject = ['$scope'];
- function ChildShoppingController($scope){
-   $scope.$on('parent', function (event, data) {
-    if(data > 0){
-      $scope.AlreadyBroughtMessage = '';
-    }
-  });
+
+ ChildShoppingController.$inject = ['ShoppingListCheckOffService'];
+ function ChildShoppingController(ShoppingListCheckOffService){
+     var child = this;
+     child.message ='Nothing bought yet';
+     child.items = ShoppingListCheckOffService.getItemsBought();     
  };
+
+ function ShoppingListCheckOffService(){
+   var service = this;
+   var toBuyList = [
+     {name:"Milk", quantity :10},
+     {name:"Donuts", quantity :12},
+     {name:"Pepto Bismol", quantity :5},
+     {name:"Cookies", quantity :20},
+     {name:"Chocolate", quantity :10}
+   ];
+   var boughtItemsList = [];
+   service.getItemsToBuy = function(){
+     return toBuyList;
+   };
+
+   service.getItemsBought = function(){
+     return boughtItemsList;
+   };
+
+   service.buyingItem = function(item){
+     boughtItemsList.push(item);
+     var index = toBuyList.indexOf(item);
+     toBuyList.splice(index,1);
+   }
+ }
 
 })();
